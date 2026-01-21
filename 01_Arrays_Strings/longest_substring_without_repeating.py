250025e5d860dacf3ed604af108e86af6d4e3b66
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2026-01-21 23:30:25 +0530

    Create longest_substring_without_repeating.py

---
+++ 01_Arrays_Strings/longest_substring_without_repeating.py
@@ -0,0 +1,22 @@
+"""
+Problem: Longest Substring Without Repeating Characters
+Technique: Sliding Window + Set
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_set = set()
+        left = 0
+        max_length = 0
+
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+
+            char_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
