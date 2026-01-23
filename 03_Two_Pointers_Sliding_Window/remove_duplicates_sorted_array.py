29fbe7da33d5e988600aedc539362728c771a626
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2026-01-24 01:11:36 +0530

    Create remove_duplicates_sorted_array.py

---
+++ 03_Two_Pointers_Sliding_Window/remove_duplicates_sorted_array.py
@@ -0,0 +1,22 @@
+"""
+Problem: Remove Duplicates from Sorted Array
+Technique: Two Pointers
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        slow = 0
+
+        for fast in range(1, len(nums)):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+
+        return slow + 1
