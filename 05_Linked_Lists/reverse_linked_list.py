e66142790426e9a54017567fb726c953a15dd497
Shyam Vyawahare <Shyamvyawahare1@gmail.com>
2026-01-27 23:14:02 +0530

    Update reverse_linked_list.py

--- 05_Linked_Lists/reverse_linked_list.py
+++ 05_Linked_Lists/reverse_linked_list.py
@@ -1,8 +1,19 @@
 """
-Concept: Singly Linked List - Basic Structure
+Problem: Reverse Linked List
+Technique: Iterative Pointer Manipulation
+Time Complexity: O(n)
+Space Complexity: O(1)
 """
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+class Solution:
+    def reverseList(self, head):
+        prev = None
+        curr = head
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        return prev
